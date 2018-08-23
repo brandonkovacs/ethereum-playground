619b531af45ad9b18498d6a9adb33cf43ce06224
Brandon Kovacs <brandon.kovacs@gmail.com>
2018-08-22 19:36:10 -0700

    simple string storage contract

---
+++ contracts/Storage.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.0;
+
+contract Storage {
+    string _data;
+    address _creator;
+
+    constructor() public {
+        _creator = msg.sender;
+    }
+
+    function setData(string text) public {
+        _data = text;
+    }
+
+    function getData() public constant returns (string) {
+        return _data;
+    }
+}
